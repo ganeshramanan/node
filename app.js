7b0a7deab2cb9969c0c39697633a345dee796f1b
unknown <tganeshramanan85@gmail.com>
2021-05-09 10:27:58 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res) => {
+res.send("Welcome to home page node");
+});
+
+app.listen(3000, () =>{
+console.log("Welcome node");
+});
+
+
+
+
